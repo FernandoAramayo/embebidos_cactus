3c6a269d230051f44d570f93eaed4bea4f9e45b8
montanocmmc <montanocmmc@gmail.com>
2026-04-30 17:20:20 -0400

    Ej 1 y 5

---
+++ lab8/LAB 8_5.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+imagen = cv2.imread('download.jpeg')
+
+if imagen is None:
+    print("Error")
+else:
+    gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+
+    _, binarizada = cv2.threshold(gris, 127, 255, cv2.THRESH_BINARY)
+
+    contornos, jerarquia = cv2.findContours(binarizada, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    cv2.drawContours(imagen, contornos, -1, (0, 255, 0), 2)
+
+    cv2.imshow('Ventana de Salida', imagen) 
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
